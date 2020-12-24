c3619adb3a49b2d14d406a5c068dd3f0ce933e08
WerpNSK <mininnick@yandex.ru>
2020-12-24 16:11:19 +0700

    Добавьте файлы проекта.

---
+++ TimerToClose/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <ctime>
+using namespace std;
+
+
+void main() {
+	setlocale(0, "");
+
+	cout << "Введите время для отсчета" << endl;
+	int time;
+	int sec;
+	int  minute;
+	int hours;
+	cin >> time;
+	
+	while (time != 0) {
+		time--;
+		sec = time;
+		hours = sec / 3600;
+		sec = sec - hours * 3600;
+		minute = sec / 60;
+		sec = sec % 60;
+		cout << "Осталось: " << hours << " : " << minute << " : " << sec;
+		Sleep(1000);
+		system("cls");
+	}
+
+
+	system("close");
+}
